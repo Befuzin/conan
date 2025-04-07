ae1a75edeeb74030361a24da8489dd97b7ecfa5c
Befuzin <befuzin@gmail.com>
2025-04-07 17:05:13 +0200

    Add files via upload

---
+++ dreaming/module/helpers/config.mjs
@@ -0,0 +1,21 @@
+export const DREAMING = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+DREAMING.abilities = {
+  might: 'DREAMING.Ability.Might.long',
+  edge: 'DREAMING.Ability.Edge.long',
+  grit: 'DREAMING.Ability.Grit.long',
+  wits: 'DREAMING.Ability.Wits.long',
+  flex: 'DREAMING.Ability.Flex.long',
+};
+
+DREAMING.abilityAbbreviations = {
+  might: 'DREAMING.Ability.Might.abbr',
+  edge: 'DREAMING.Ability.Edge.abbr',
+  grit: 'DREAMING.Ability.Grit.abbr',
+  wits: 'DREAMING.Ability.Wits.abbr',
+  flex: 'DREAMING.Ability.Flex.abbr',
+};
